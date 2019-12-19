86b0bb8a3b614dd45d6c8ebffa0c0602e6b64477
Francisco Pérez <frperezr93@gmail.com>
2019-12-18 22:24:55 -0300

    add imageUrl to schema

--- src/graphql/country/resolvers/models/continent.js
+++ src/graphql/country/resolvers/models/continent.js
@@ -2,6 +2,7 @@ module.exports = {
   id: ({ id }) => id,
   code: ({ code }) => code,
   name: ({ name }) => name,
+  imageUrl: ({ imageUrl }) => imageUrl,
   createdAt: ({ createdAt }) => createdAt,
   updatedAt: ({ updatedAt }) => updatedAt,
 }
